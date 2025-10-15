ee7cca6bd310132d55b206ce8bfb0895188a8f1a
ASIA\Willy_Chang <d410413@gmail.com>
2025-10-15 11:05:56 +0800

    save local changes before switching branch

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: process.env.NODE_ENV === 'production' ? '/line-link-generator/' : '/',
+  base: '/line-link-generator/',   // 🔹 直接寫死
 
     plugins: [react(), tailwindcss()],
 
